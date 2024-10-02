8d68ee30373ea188d46f8e3671021cc5ccb158cd
Samuel <samuel.gfries@gmail.com>
2024-10-02 19:16:46 -0300

    updt final raiox

--- 2024/src/raiox.c
+++ 2024/src/raiox.c
@@ -1,8 +1,14 @@
+#include <math.h>
 #include "raiox.h"
+#include "cinza.h"
 
 void xray(PPMImage *img, float factor) {
+    // Primeiro converte para tons de cinza
+    grayscale(img);
+
+    // Em seguida, aplica o efeito de raio-X
     for (int i = 0; i < img->width * img->height * 3; i++) {
-        int value = img->data[i] * factor;
-        img->data[i] = value > 255 ? 255 : value;
+        int value = pow(img->data[i], factor); // Eleva o valor ao fator desejado
+        img->data[i] = value > 255 ? 255 : value; // Limita o valor máximo a 255
     }
 }
